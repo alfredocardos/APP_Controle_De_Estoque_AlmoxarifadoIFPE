c104d2ef43bccfc338952fac1d2fe57a861505ad
Alfredo_Javeiro777 <102482783+alfredocardos@users.noreply.github.com>
2022-07-07 14:35:29 -0300

    Update3.0

--- src/C_VIEW/Teste.java
+++ src/C_VIEW/Teste.java
@@ -8,11 +8,11 @@ public class Teste {
     private  static  final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        {   System.out.println("***********************************************************");
-            System.out.println("         BEM VINDO AO ALMOXARIFADO VIRTUAL IFPE            ");
-            System.out.println("***********************************************************");
-            System.out.println("ELEMENTOS DE DESPESAS PARA REALIZAR MOVIMENTAÇÕES LOGISTICAS");
-            System.out.println("***********************************************************");
+        {   System.out.println("**************************************************************");
+            System.out.println("          BEM VINDO AO ALMOXARIFADO VIRTUAL IFPE               ");
+            System.out.println("**************************************************************");
+            System.out.println(" ELEMENTOS DE DESPESAS PARA REALIZAR MOVIMENTAÇÕES LOGISTICAS ");
+            System.out.println("**************************************************************");
 
 
         }
